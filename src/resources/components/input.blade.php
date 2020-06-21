35c25c31c2dafe09040ac5ca7442837e57477069
Jalal Uddin <dgvai@users.noreply.github.com>
2020-06-21 11:12:41 +0600

    ✔ Added some html attrs in input

--- src/resources/components/input.blade.php
+++ src/resources/components/input.blade.php
@@ -2,6 +2,7 @@
     <label for="{{$id}}">{{$label}}</label>
     <input type="{{$type}}" class="{{$inputclass}} form-control @error($name) is-invalid @enderror" 
     id="{{$id}}" name="{{$name}}" placeholder="{{$placeholder}}" 
+    step="{{$step}}" max="{{$max}}" maxlength="{{$maxlength}}" pattern="{{$pattern}}"
     value="{{$value}}" 
     {{($required) ? 'required' : '' }} 
     {{($disabled) ? 'disabled' : '' }}>
